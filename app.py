1c886e20c6274b6ef142316990837f3764ff3269
Hamza Arshad <hamzaarshad223@gmail.com>
2023-07-26 23:18:23 +0500

    added app and api views

---
+++ app.py
@@ -0,0 +1,81 @@
+from flask import Flask, render_template, request, jsonify
+from openai_api.chatgpt_api import ChatGptBotAPI
+from config import *
+
+app = Flask(__name__)
+chatgpt_api = ChatGptBotAPI(OPENAI_API_KEY)
+
+@app.route('/')
+def index():
+    data = {
+        'prompts': chatgpt_api.get_prompts()
+    }
+    return render_template('index.html', data=data)
+
+@app.route('/create-prompt', methods=['POST'])
+def create_prompt():
+    """
+    create a prompt message using the input received 
+    from the user.
+    """
+
+    data = request.get_json()  # Extract the JSON data from the request
+    text = data.get('text')    # Get the 'text' field from the JSON data
+    # Do something with the submitted text (e.g., save it to a database)
+    # Replace the following line with your desired processing logic
+    prompt = chatgpt_api.create_prompt(text)
+    if prompt:
+        result = {
+            'status': 'success',
+            'result': prompt}
+        return jsonify(result), 200
+    else:
+        result = {
+            'status': 'fail'
+        }
+        return jsonify(result), 400
+
+
+@app.route('/delete-prompt/<item_id>', methods=['DELETE'])
+def delete_prompt(item_id):
+    # Your code to delete the item with the given ID
+    # Example: Delete item with ID from a database
+    # return some response
+    try:
+        prompt_index = int(item_id)
+        if chatgpt_api.delete_prompt(prompt_index):
+            return jsonify({'message': f'Item with ID {item_id} deleted successfully', 'status': 'success'}), 200
+    except Exception as e:
+        return jsonify({'message': str(e), 'status': 'fail'}), 400
+
+@app.route('/update-prompt/<item_id>', methods=['PUT'])
+def update_prompt(item_id):
+    # Your code to delete the item with the given ID
+    # Example: Delete item with ID from a database
+    # return some response
+    try:
+        prompt_index = int(item_id)
+        data = request.get_json()
+        text = data.get('text')
+        updated_prompt = chatgpt_api.update_prompt(prompt_index, text)
+        if updated_prompt:
+            return jsonify({'result': updated_prompt, 'status': 'success'}), 200
+    except Exception as e:
+        return jsonify({'message': str(e), 'status': 'fail'}), 400
+
+@app.route('/generate-response/<item_id>', methods=['GET'])
+def generate_response(item_id):
+    # Your code to delete the item with the given ID
+    # Example: Delete item with ID from a database
+    # return some response
+    try:
+        prompt_index = int(item_id)
+        
+        updated_prompt = chatgpt_api.get_response(prompt_index)
+        if updated_prompt:
+            return jsonify({'result': updated_prompt, 'status': 'success'}), 200
+    except Exception as e:
+        return jsonify({'message': str(e), 'status': 'fail'}), 400
+
+if __name__ == '__main__':
+    app.run()
---
+++ openai_api/chatgpt_api.py
@@ -0,0 +1,42 @@
+import openai
+
+class ChatGptBotAPI:
+    def __init__(self, open_api_key):
+        self.api_key = open_api_key
+        self.model = "gpt-3.5-turbo"
+        self.prompts = []
+
+    def get_prompts(self):
+        return self.prompts
+
+    def create_prompt(self, prompt):
+        try:
+            if len(prompt) > 0:
+                prompt_obj = {"role": "user", "prompt": prompt, 'response': ''}
+                
+                self.prompts.append(prompt_obj)
+                self.prompts[-1]['id'] = len(self.prompts) - 1
+                return prompt_obj
+            else:
+                return False
+        except:
+            return False
+        
+    def get_response(self, prompt_index):
+        response = openai.ChatCompletion.create(
+            model = self.model,
+            messages = self.prompts[prompt_index]['prompt']
+        )
+        ChatGPT_reply = response["choices"][0]["message"]["content"]
+        self.prompts[prompt_index]['response'] = ChatGPT_reply
+
+        return ChatGPT_reply
+
+    def update_prompt(self, prompt_index, new_prompt):
+        self.prompts[prompt_index]['prompt'] = new_prompt
+        return {'prompt': self.prompts[prompt_index]['prompt'], 'id': prompt_index}
+
+    def delete_prompt(self, prompt_index):
+        return self.prompts.pop(prompt_index)
+
+
